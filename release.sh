f9a30eff7d575e283340a2d99e040935a6589799
xialingsc <xialingsc@gmail.com>
2015-11-11 23:36:23 +0800

    release post

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 
 git add -A && git commit -m 'release post' 
-git push origin gh-pages
+#git push origin gh-pages
+git push origin github.com:xialingsc/gh-pages
+#git push git@github.com:xialingsc/autobuildbaseimage.git
