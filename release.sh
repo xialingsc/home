acc99daf0b2c248bae5a066c723bdb1919b1e4ac
xialingsc <xialingsc@gmail.com>
2015-11-12 10:33:37 +0800

    release post

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
+git remote add github git@github.com:xialingsc/home.git
 git add -A && git commit -m 'release post' 
-git push origin gh-pages
+git push github gh-pages
 #git push origin github.com:xialingsc/gh-pages
 #git push git@github.com:xialingsc/autobuildbaseimage.git
